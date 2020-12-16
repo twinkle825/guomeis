26e85625a2d320060b95373b06fff10ffb048369
“hesangit config --global user.email 1519507780@qq.comgit config --global user.name “hesan <1519507780@qq.com>
2020-12-16 19:27:09 +0800

    何三提交测试

---
+++ static/json/hottopic.js
@@ -0,0 +1,396 @@
+var hotpic = {
+    "data": [
+        {
+            "Images": "https://gfs13.gomein.net.cn/T1dLb4B4Zv1RCvBVdK.jpg",
+            "title": "小清风格照到底要怎么拍",
+            "look": "1.4万",
+            "head": "https://gfs11.gomein.net.cn/T18yJjBTVT1RCvBVdK_27_27.jpg",
+            "name": "相信无限",
+            "zan": "713"
+        },
+        {
+            "Images": "https://gfs13.gomein.net.cn/T16hJ4Bbdv1RCvBVdK.jpg",
+            "title": "气温攀升让你想起了什么事？",
+            "look": "1.2万",
+            "head": "https://gfs11.gomein.net.cn/T19udvBXDg1RCvBVdK_27_27.jpg",
+            "name": "手心的太阳月亮",
+            "zan": "619"
+        },
+        {
+            "Images": "https://gfs13.gomein.net.cn/T1WeA4BbWv1RCvBVdK.jpg",
+            "title": "叫人赞不绝口直白吹风机",
+            "look": "1.8万",
+            "head": "https://gfs10.gomein.net.cn/T1p.LbBXJT1RCvBVdK_27_27.jpg",
+            "name": "iambill",
+            "zan": "582"
+        },
+        {
+            "Images": "https://gfs12.gomein.net.cn/T1ugC4B4Ev1RCvBVdK.jpg",
+            "title": "为什么娃睡觉会出虚汗？",
+            "look": "1.2万",
+            "head": "https://gfs11.gomein.net.cn/T18yJjBTVT1RCvBVdK_27_27.jpg",
+            "name": "相信无限",
+            "zan": "620"
+        },
+        {
+            "Images": "https://gfs13.gomein.net.cn/T1iSV4BQVT1RCvBVdK.jpg",
+            "title": "这个季节选购浴霸便宜吗？",
+            "look": "1.2万",
+            "head": "https://gfs11.gomein.net.cn/T19udvBXDg1RCvBVdK_27_27.jpg",
+            "name": "手心的太阳月亮",
+            "zan": "618"
+        },
+        {
+            "Images": "https://gfs10.gomein.net.cn/T1mwZ4B5KT1RCvBVdK.jpg",
+            "title": "把好喝的葡萄酒装进口袋里",
+            "look": "1.2万",
+            "head": "https://gfs13.gomein.net.cn/T18bKjBmbv1RCvBVdK_27_27.jpg",
+            "name": "简简单单不好嘛",
+            "zan": "596"
+        },
+        {
+            "Images": "https://gfs13.gomein.net.cn/T1pPV4Bmhv1RCvBVdK.jpg",
+            "title": "肚子上的肥肉在冲你微笑",
+            "look": "1.2万",
+            "head": "https://gfs10.gomein.net.cn/T1ZrLvBjJT1RCvBVdK_27_27.jpg",
+            "name": "允伊",
+            "zan": "572"
+        },
+        {
+            "Images": "https://gfs11.gomein.net.cn/T1sKL4BQDv1RCvBVdK.jpg",
+            "title": "掌握这几招儿单反保养知识",
+            "look": "1.2万",
+            "head": "https://gfs11.gomein.net.cn/T19udvBXDg1RCvBVdK_27_27.jpg",
+            "name": "手心的太阳月亮",
+            "zan": "627"
+        },
+        {
+            "Images": "https://gfs11.gomein.net.cn/T1YvJ4BC__1RCvBVdK.png",
+            "title": "大风天教会我了这些道理",
+            "look": "1.2万",
+            "head": "https://gfs13.gomein.net.cn/T18bKjBmbv1RCvBVdK_27_27.jpg",
+            "name": "简简单单不好嘛",
+            "zan": "621"
+        },
+        {
+            "Images": "https://gfs10.gomein.net.cn/T1_KJ4BQxv1RCvBVdK.jpg",
+            "title": "笔记本电脑怎么选？并不难",
+            "look": "1.2万",
+            "head": "https://gfs10.gomein.net.cn/T18TdQBTWT1RCvBVdK_27_27.jpg",
+            "name": "就是爱电脑",
+            "zan": "586"
+        }
+    ],
+    "life": [
+        {
+            "Images": "https://gfs13.gomein.net.cn/T1vXJ4Bj_v1RCvBVdK.jpg",
+            "title": "如何提升在家防疫的幸福感?",
+            "content": "说好的在家防疫并不是一句空话，了解对抗疫情战的武器才能取得最后的胜利。",
+            "look": "1.6万",
+            "zan": "612",
+            "head": "https://gfs10.gomein.net.cn/T18TdQBTWT1RCvBVdK_219_138.jpg",
+            "id": "就是爱电脑",
+            "simg": [
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1I7YbBTVT1RCvBVdK_60_60.jpg",
+                    "price": "2999.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1K5WyBvET1RCvBVdK_60_60.jpg",
+                    "price": "99.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1A0KjBKDv1RCvBVdK_60_60.jpg",
+                    "price": "1599.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1xfhCBCDT1RCvBVdK_60_60.jpg",
+                    "price": "4299.00"
+                },
+            ]
+        },
+        {
+            "Images": "https://gfs10.gomein.net.cn/T1jFh4BmKv1RCvBVdK.jpg",
+            "title": "防控疫情除了勤洗手，你还需要做这项消毒工作",
+            "content": "问题来了，我们宅在家里如何科学消毒才能有效避免病从口入呢？",
+            "look": "2.3万",
+            "zan": "601",
+            "head": "https://gfs11.gomein.net.cn/T19udvBXDg1RCvBVdK_219_138.jpg",
+            "id": "手心的太阳月亮",
+            "simg": [
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1YSEyBgVT1RCvBVdK_60_60.jpg",
+                    "price": "799.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1PaV4BKCv1RCvBVdK_60_60.jpg",
+                    "price": "2399.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T139dCBvdv1RCvBVdK_60_60.jpg",
+                    "price": "308.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1k6hCB4_v1RCvBVdK_60_60.jpg",
+                    "price": "1499.00"
+                },
+            ],
+        },
+        {
+            "Images": "https://gfs10.gomein.net.cn/T16dWXBgLv1RCvBVdK.jpg",
+            "title": "提升工作舒适度，这些东西记得入手",
+            "content": "对于长期坐在办公室的白领而言，把工位有限的空间打造的精致舒适，上班的心情也会好很多。",
+            "look": "2.3万",
+            "zan": "635",
+            "head": "https://gfs10.gomein.net.cn/T18TdQBTWT1RCvBVdK_219_138.jpg",
+            "id": "就是爱电脑",
+            "simg": [
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1RlZjBTZT1RCvBVdK_60_60.jpg",
+                    "price": "169.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T196DTByx_1RCvBVdK_60_60.jpg",
+                    "price": "89.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1EKdvBXJ_1RCvBVdK_60_60.jpg",
+                    "price": "33.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1QcZbByV_1RCvBVdK_60_60.jpg",
+                    "price": "149.00"
+                },
+            ],
+        },
+        {
+            "Images": "https://gfs12.gomein.net.cn/T1L1YsBvxv1RCvBVdK.jpg",
+            "title": "巧用厨房电器打造健康厨房",
+            "content": "厨房作为一切美食产生的源头，看似是一个非常安全的地方，却存在着很多的健康隐患。",
+            "look": "2.4万",
+            "zan": "626",
+            "head": "https://gfs12.gomein.net.cn/T1fWZgBjdv1RCvBVdK_219_138.jpg",
+            "id": "桔子生活",
+            "simg": [
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1eHDbBKCv1RCvBVdK_60_60.jpg",
+                    "price": "2849.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1K_xsBvVv1RCvBVdK_60_60.jpg",
+                    "price": "1799.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1BdWjB7Cv1RCvBVdK_60_60.jpg",
+                    "price": "1599.00"
+                },
+                {
+                    "img": "https://gfs17.gomein.net.cn/T1W5_sB4Y_1RCvBVdK_60_60.jpg",
+                    "price": "2399.00"
+                },
+            ],
+        }
+    ],
+    "shop": [
+        //美食
+        {
+            "title": "美食",
+            "lunbo": [
+                {
+                    "img": "https://gfs11.gomein.net.cn/T12NCsBvCv1RCvBVdK.jpg",
+                    "cont": "11.90"
+                },
+                {
+                    "img": "https://gfs11.gomein.net.cn/T1WTAsBT_v1RCvBVdK.jpg",
+                    "cont": "21.90"
+                },
+                {
+                    "img": "https://gfs10.gomein.net.cn/T183JjBb_v1RCvBVdK.jpg",
+                    "cont": "68.00"
+                },
+                {
+                    "img": "https://gfs12.gomein.net.cn/T1UFEjBjLv1RCvBVdK.jpg",
+                    "cont": "16.89"
+                },
+            ],
+            "simg": [
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1uYCQBCdv1RCvBVdK_160_160.jpg",
+                    "price": "32.79",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T10X_jBXxv1RCvBVdK_160_160.jpg",
+                    "price": "12.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1dPL_BgCT1RCvBVdK_160_160.jpg",
+                    "price": "25.80",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1LbDbBCCT1RCvBVdK_160_160.jpg",
+                    "price": "1.50",
+                },
+            ],
+        },
+        // 家电
+        {
+            "title": "家电",
+            "lunbo": [
+                {
+                    "img": "https://gfs10.gomein.net.cn/T11K_gBXD_1RCvBVdK.jpg",
+                    "cont": "￥4199"
+                },
+                {
+                    "img": "https://gfs12.gomein.net.cn/T13iZbBQ_v1RCvBVdK.jpg",
+                    "cont": "￥1399"
+                },
+                {
+                    "img": "https://gfs12.gomein.net.cn/T1mtxgBCV_1RCvBVdK.jpg",
+                    "cont": "￥1799"
+                },
+                {
+                    "img": "https://gfs12.gomein.net.cn/T1.aVbB7E_1RCvBVdK.jpg",
+                    "cont": "￥4199"
+                },
+            ],
+            "simg": [
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1bRY_B4ET1RCvBVdK_160_160.jpg",
+                    "price": "1198.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T10r_gB7bv1RCvBVdK_160_160.jpg",
+                    "price": "3999.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1MpYsBXVT1RCvBVdK_160_160.jpg",
+                    "price": "3499.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1t8KsB7CT1RCvBVdK_160_160.jpg",
+                    "price": "3099.00",
+                },
+            ],
+        },
+        // 家居
+        {
+            "title": "家居",
+            "lunbo": [
+                {
+                    "img": "https://gfs12.gomein.net.cn/T1qqZbB4Z_1RCvBVdK.jpg",
+                    "cont": "3570.00"
+                },
+                {
+                    "img": "https://gfs13.gomein.net.cn/T1bnCbB_Lv1RCvBVdK.jpg",
+                    "cont": "1400.00"
+                },
+                {
+                    "img": "https://gfs13.gomein.net.cn/T1buWbB_Dv1RCvBVdK.jpg",
+                    "cont": "128.00"
+                },
+                {
+                    "img": "https://gfs12.gomein.net.cn/T1EaWgBQJv1RCvBVdK.jpg",
+                    "cont": "29.90"
+                },
+            ],
+            "simg": [
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T12kWvBKKj1RCvBVdK_160_160.jpg",
+                    "price": "1251.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1cVWvB4bQ1RCvBVdK_160_160.jpg",
+                    "price": "1676.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1aiDvBKLv1RCvBVdK_160_160.jpg",
+                    "price": "3999.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1mLEvBmAj1RCvBVdK_160_160.jpg",
+                    "price": "799.00",
+                },
+            ],
+        },
+        // 服饰
+        {
+            "title": "服饰",
+            "lunbo": [
+                {
+                    "img": "https://gfs12.gomein.net.cn/T1HPJsB7Zv1RCvBVdK.jpg",
+                    "cont": "168.00"
+                },
+                {
+                    "img": "https://gfs13.gomein.net.cn/T1zWxvBjEv1RCvBVdK.jpg",
+                    "cont": "55.00"
+                },
+                {
+                    "img": "https://gfs13.gomein.net.cn/T1egJsBmDT1RCvBVdK.jpg",
+                    "cont": "11.00"
+                },
+                {
+                    "img": "https://gfs11.gomein.net.cn/T1peWsB5Cv1RCvBVdK.jpg",
+                    "cont": "299.00"
+                },
+            ],
+            "simg": [
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1Hcx_BsbT1RCvBVdK_160_160.jpg",
+                    "price": "178.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1MoDQBXVg1RCvBVdK_160_160.jpg",
+                    "price": "29.90",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1oiCvBTVT1RCvBVdK_160_160.jpg",
+                    "price": "43.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1Tu_vBQdv1RCvBVdK_160_160.jpg",
+                    "price": "19.89",
+                },
+            ],
+        },
+        // 潮3c
+        {
+            "title": "潮3c",
+            "lunbo": [
+                {
+                    "img": "https://gfs13.gomein.net.cn/T1XAKsB_ZT1RCvBVdK.jpg",
+                    "cont": "2799.00"
+                },
+                {
+                    "img": "https://gfs13.gomein.net.cn/T1EvLsB4YT1RCvBVdK.jpg",
+                    "cont": "399.00"
+                },
+                {
+                    "img": "https://gfs11.gomein.net.cn/T1wgKsBgVv1RCvBVdK.jpg",
+                    "cont": "5699.00"
+                },
+                {
+                    "img": "https://gfs10.gomein.net.cn/T1jnKsBmAv1RCvBVdK.jpg",
+                    "cont": "399.00"
+                },
+            ],
+            "simg": [
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1bMJTB__T1RCvBVdK_160_160.jpg",
+                    "price": "1699.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T18hWvBvWg1RCvBVdK_160_160.jpg",
+                    "price": "1038.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1VmCvB7Cb1RCvBVdK_160_160.jpg",
+                    "price": "7599.00",
+                },
+                {
+                    "pic": "https://gfs17.gomein.net.cn/T1n8D_B5VT1RCvBVdK_160_160.jpg",
+                    "price": "1999.00",
+                },
+            ],
+        },
+    ]
+}
